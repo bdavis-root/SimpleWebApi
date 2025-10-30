8960b00991b3b34285491aa4a9641c5191b82088
bdavis-root <99501179+bdavis-root@users.noreply.github.com>
2025-10-30 08:40:22 -0500

    Initial commit.

---
+++ SimpleWebApi/Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+﻿using Azure.Core.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SimpleWebApi.Data;
+using SimpleWebApi.Models;
+
+namespace SimpleWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController(SimpleWebApiContext context) : ControllerBase
+    {
+        private readonly SimpleWebApiContext _context = context;
+
+        // GET: api/Users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUser()
+        {
+            return await _context.User.ToListAsync();
+        }
+
+        // GET: api/Users/2
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            var user = await _context.User.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return user;
+        }
+
+        // Example error. Should not provide stack trace to caller due to GlogalExceptionHandler.
+        [HttpGet("error")]
+        public Task<ActionResult<User>> ThrowError()
+        {
+            throw new Exception("This is an example error.");
+        }
+    }
+}
---
+++ SimpleWebApi/Exceptions/GlobalExceptionHandler.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleWebApi.Exceptions
+{
+    public class GlobalExceptionHandler(IProblemDetailsService problemDetailsService, ILogger<GlobalExceptionHandler> logger) : IExceptionHandler
+    {
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        {
+            //Add TraceIdentifier to exception data for logging.
+            exception.Data.Add("TraceID", httpContext.TraceIdentifier);
+            logger.LogError(exception, "Unhandled exception occurred");
+            httpContext.Response.StatusCode = exception switch
+            {
+                ApplicationException => StatusCodes.Status400BadRequest,
+                _ => StatusCodes.Status500InternalServerError
+            };
+
+            return await problemDetailsService.TryWriteAsync(new ProblemDetailsContext
+            {
+                HttpContext = httpContext,
+                Exception = exception,
+                ProblemDetails = new ProblemDetails
+                {
+                    Type = exception.GetType().Name,
+                    Title = "An error occurred while processing your request",
+                    Detail = exception.Message
+                }
+            });
+        }
+    }
+}
